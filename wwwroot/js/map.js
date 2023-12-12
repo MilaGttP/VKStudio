67f2746802e028ce4c6a3f2c79a6421fd600f04e
MilaGttP <milaivanova2609@gmail.com>
2023-12-12 22:09:04 +0200

    Added items to wwwroot

---
+++ wwwroot/js/map.js
@@ -0,0 +1,10 @@
+﻿document.getElementById('showImage').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.body.classList.add('no-scroll');
+    document.getElementById('hiddenImageContainer').style.left = '0';
+});
+
+document.getElementById('closeButton').addEventListener('click', function() {
+    document.body.classList.remove('no-scroll');
+    document.getElementById('hiddenImageContainer').style.left = '-100%';
+});
