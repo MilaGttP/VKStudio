3513c9327c7ab5ab144038449659c18a97541178
MilaGttP <milaivanova2609@gmail.com>
2023-12-12 22:08:48 +0200

    Added all pages and completed index

---
+++ Pages/ChoosePhotos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VKStudio.Pages
+{
+    public class ChoosePhotosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VKStudio.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
