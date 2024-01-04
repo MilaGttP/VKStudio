144460c67f5a489fb5e5b215d201dab6c172d4a0
MilaGttP <milaivanova2609@gmail.com>
2024-01-04 17:40:37 +0200

    add services

---
+++ Services/IAuthService.cs
@@ -0,0 +1,25 @@
+﻿namespace VKStudio.Services
+{
+    public interface IAuthService
+    {
+        bool IsValidUser(string login, string password);
+    }
+
+    public class AuthService : IAuthService
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public bool IsValidUser(string login, string password)
+        {
+            string expectedLogin = _configuration["UserSecrets:Login"];
+            string expectedPassword = _configuration["UserSecrets:Password"];
+
+            return login == expectedLogin && password == expectedPassword;
+        }
+    }
+}
---
+++ Services/IImageService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VKStudio.Models;
+
+namespace VKStudio.Services
+{
+    public class IImageService
+    {
+        private readonly StudioDBContext _dbContext = new StudioDBContext(new DbContextOptions<StudioDBContext>());
+
+        public List<Image> GetVisibleImages()
+        {
+            return _dbContext.Images.Where(i => i.IsVisible).ToList();
+        }
+
+        public List<Image> GetUnvisibleImages()
+        {
+            return _dbContext.Images.Where(i => !i.IsVisible).ToList();
+        }
+
+        public List<Image> GetCutImages()
+        {
+            return _dbContext.Images.Where(i => i.Name.StartsWith("cut")).ToList();
+        }
+
+        public List<Image> GetDyedImages()
+        {
+            return _dbContext.Images.Where(i => i.Name.StartsWith("dyed")).ToList();
+        }
+
+        public List<Image> GetProdImages()
+        {
+            return _dbContext.Images.Where(i => i.Name.StartsWith("prod")).ToList();
+        }
+
+        public List<Image> GetVisibleCutImages()
+        {
+            return _dbContext.Images.Where(i => i.Name.StartsWith("cut") && i.IsVisible).ToList();
+        }
+
+        public List<Image> GetVisibleDyedImages()
+        {
+            return _dbContext.Images.Where(i => i.Name.StartsWith("dyed") && i.IsVisible).ToList();
+        }
+
+        public List<Image> GetVisibleProdImages()
+        {
+            return _dbContext.Images.Where(i => i.Name.StartsWith("prod") && i.IsVisible).ToList();
+        }
+
+        public bool ToggleImageVisibility(int imageId, bool isVisible)
+        {
+            var image = _dbContext.Images.FirstOrDefault(i => i.ID == imageId);
+
+            if (image != null)
+            {
+                image.IsVisible = isVisible;
+                _dbContext.SaveChanges();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
