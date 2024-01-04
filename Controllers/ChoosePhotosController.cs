0fa495fc8cc430567500797f04a4249751a9b6f2
MilaGttP <milaivanova2609@gmail.com>
2024-01-04 17:40:08 +0200

    add controllers

---
+++ Controllers/ChoosePhotosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VKStudio.Services;
+
+namespace VKStudio.Controllers
+{
+    public class ChoosePhotosController : Controller
+    {
+        private readonly IImageService _imageService;
+
+        public ChoosePhotosController(IImageService imageService)
+        {
+            _imageService = imageService;
+        }
+
+        [HttpPost]
+        public JsonResult ToggleImageVisibility(int imageId, bool isVisible)
+        {
+            bool result = _imageService.ToggleImageVisibility(imageId, isVisible);
+            return Json(new { success = result });
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using VKStudio.Models;
+using VKStudio.Services;
+
+namespace VKStudio.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public LoginController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginRequest loginRequest)
+        {
+            try
+            {
+                if (_authService.IsValidUser(loginRequest.Login, loginRequest.Password))
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, loginRequest.Login),
+                    };
+
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var principal = new ClaimsPrincipal(identity);
+
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                    return Ok(new { success = true });
+                }
+
+                return BadRequest(new { success = false });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { success = false });
+            }
+        }
+    }
+}
