61e964018940c85f705769729645e8fcfc50ce36
MilaGttP <milaivanova2609@gmail.com>
2024-01-04 22:43:17 +0200

    update wwwroot

---
+++ wwwroot/js/passwordEye.js
@@ -0,0 +1,12 @@
+﻿function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("passwordInput");
+    var togglePassword = document.getElementById("togglePassword");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text"; 
+        togglePassword.innerHTML = "&#10006;";
+    } else {
+        passwordInput.type = "password";
+        togglePassword.innerHTML = "&#128065;";
+    }
+}
---
+++ wwwroot/js/slides.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const track = document.getElementById("carouselTrack");
+    let slides = document.querySelectorAll(".carousel-slide");
+    let index = 0;
+
+    function updateCarousel() {
+        if (slides[index]) {
+            const size = slides[index].clientWidth;
+            track.style.transform = `translateX(${-size * index}px)`;
+        }
+    }
+
+    function nextSlide() {
+        index = (index + 1) % slides.length;
+        updateCarousel();
+    }
+
+    setInterval(nextSlide, 3000);
+});
