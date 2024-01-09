b02d8267d292f53d7103bec6cc25c334db7c84aa
MilaGttP <milaivanova2609@gmail.com>
2024-01-09 22:22:41 +0200

    updated js

---
+++ wwwroot/js/burgerMenu.js
@@ -0,0 +1,20 @@
+﻿function toggleBurgerMenu() {
+    const burgerIcon = document.getElementById('burgerIcon');
+    const burgerMenu = document.getElementById('burgerMenu');
+    const darkBackground = document.getElementById('darkBackground');
+
+    burgerIcon.classList.toggle('open');
+    burgerMenu.style.display = burgerMenu.style.display === 'block' ? 'none' : 'block';
+    darkBackground.classList.toggle('open');
+}
+
+document.addEventListener('click', function (event) {
+    const burgerMenu = document.getElementById('burgerMenu');
+    const darkBackground = document.getElementById('darkBackground');
+
+    if (!burgerMenu.contains(event.target) && event.target.id !== 'burgerIcon') {
+        burgerMenu.style.display = 'none';
+        document.getElementById('burgerIcon').classList.remove('open');
+        darkBackground.classList.remove('open');
+    }
+});
